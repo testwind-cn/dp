f3eeb81cce4e8f340b1aa1d937624d5b667ac3da
testwind-cn <testwind@sina.com>
2018-02-06 23:20:16 +0800

    2-6

--- calValue/GetRates.php
+++ calValue/GetRates.php
@@ -10,13 +10,17 @@ class TheRates
     private $d3_total_Period = 0;
 
     
-    private $first_z_pai=1;
-    private $sum_z_pai=0;
+    private $first_z_PI_day=1;
+    private $sum_z_PI_day=0;
+    
+    private $first_z_PI_per=1;
+    private $sum_z_PI_per=0;
     
 
-    private $data_z_R;                            // 当前期的息率 = 年率 /360* 本期天数
-    private $data_z_pai;                            // <1+当前期的息率>连乘积
+    private $data_z_R_day;                            // 当前期的息率 = 年率 /360* 本期天数
+    private $data_z_PI_day;                            // <1+当前期的息率>连乘积
     private $data_z_R_per;                          //按整期的利率
+    private $data_z_PI_per;                            // <1+整期的息率>连乘积
 //    private $data_z_ByDay;                            // 实际是按天算利息
     
     
@@ -24,18 +28,18 @@ class TheRates
     {
         //        $this->data_start_date = date_create();
         
-        $this->data_z_R = array();
-        $this->data_z_pai = array();
+        $this->data_z_R_day = array();
+        $this->data_z_PI_day = array();
         
         $this->d3_total_Period = $num;
         
-        $this->first_z_pai=1;
-        $this->sum_z_pai=0;
+        $this->first_z_PI_day=1;
+        $this->sum_z_PI_day=0;
         $this->data_z_R_per = 0;
         
         for ($i=0 ; $i<=$this->d3_total_Period+1;$i++){
-            $this->data_z_R[$i] = 0;
-            $this->data_z_pai[$i] = 1;
+            $this->data_z_R_day[$i] = 0;
+            $this->data_z_PI_day[$i] = 1;
 //            $this->data_z_ByDay[$i] = false;
         }
         
@@ -47,15 +51,15 @@ class TheRates
         echo 'Destroying: ';
         //, $this->name, PHP_EOL;
         for ($i = $this->d3_total_Period + 1; $i >= 0; $i --) {
-            if (is_array($this->data_z_R) && isset($this->data_z_R[$i]))
-                unset($this->data_z_R[$i]);
-            if (is_array($this->data_z_pai) && isset($this->data_z_pai[$i]))
-                unset($this->data_z_pai[$i]);
+            if (is_array($this->data_z_R_day) && isset($this->data_z_R_day[$i]))
+                unset($this->data_z_R_day[$i]);
+            if (is_array($this->data_z_PI_day) && isset($this->data_z_PI_day[$i]))
+                unset($this->data_z_PI_day[$i]);
 //            if (is_array($this->data_z_ByDay) && isset($this->data_z_ByDay[$i]))
 //                unset($this->data_z_ByDay[$i]);
         }
-        $this->data_z_R = null;
-        $this->data_z_pai = null;
+        $this->data_z_R_day = null;
+        $this->data_z_PI_day = null;
 //        $this->data_z_ByDay = null;
     }
     
@@ -78,53 +82,76 @@ class TheRates
         return $this->data_z_R_per;
     }
     
-    private function get_z_R($num)
+    private function set_z_R_day($num,$z1b)
+    { // 获取???
+        if ($num < 0 || $num > $this->d3_total_Period + 1)
+            return;
+            if (is_array($this->data_z_R_day) && isset($this->data_z_R_day[$num])) {
+                $this->data_z_R_day[$num] = $z1b;
+            }
+            return;
+    }
+    
+    private function get_z_R_day($num)
     { // 获取???
         if ($num < 0 || $num > $this->d3_total_Period + 1)
             return 0;
-        if (is_array($this->data_z_R) && isset($this->data_z_R[$num])) {
-            return $this->data_z_R[$num];
+        if (is_array($this->data_z_R_day) && isset($this->data_z_R_day[$num])) {
+            return $this->data_z_R_day[$num];
         }
         return 0;
     }
     
-    private function set_z_R($num,$z1b)
-    { // 获取???
-        if ($num < 0 || $num > $this->d3_total_Period + 1)
-            return;
-        if (is_array($this->data_z_R) && isset($this->data_z_R[$num])) {
-            $this->data_z_R[$num] = $z1b;
-        }
-        return;
-    }
-    
-    
-    private function get_z_pai($num)
-    { // 获取???
-        if ($num < 0 || $num > $this->d3_total_Period + 1)
-            return 1;
-        if (is_array($this->data_z_pai) && isset($this->data_z_pai[$num])) {
-            return $this->data_z_pai[$num];
-        }
-        return 1;
-    }
-    
-    private function set_z_pai($num,$mult_pai, $useSelfDay)  // = false))
+    private function set_z_PI_day($num,$mult_pai)  // = false))
     { // 获取???
         if ($num < 0 || $num > $this->d3_total_Period + 1)
             return 1;
             
-        if ( $useSelfDay ) {
-            $r = $this->get_z_R($num);
-        } else {
-            $r = $this->get_z_R_per();
-        }
-        
-        if (is_array($this->data_z_pai) && isset($this->data_z_pai[$num])) {
-            $this->data_z_pai[$num] = ( $r  + 1 ) * $mult_pai;
-        }
-        return $this->data_z_pai[$num];  // 不用return 数字的 ????
+            $r = $this->get_z_R_day($num);
+            
+            if (is_array($this->data_z_PI_day) && isset($this->data_z_PI_day[$num])) {
+                $this->data_z_PI_day[$num] = ( $r  + 1 ) * $mult_pai;
+            }
+            return $this->data_z_PI_day[$num];  // 不用return 数字的 ????
     }
+    
+    
+    private function get_z_PI_day($num)
+    { // 获取???
+        if ($num < 0 || $num > $this->d3_total_Period + 1)
+            return 1;
+        if (is_array($this->data_z_PI_day) && isset($this->data_z_PI_day[$num])) {
+            return $this->data_z_PI_day[$num];
+        }
+        return 1;
+    }
+    
+    
+    private function set_z_PI_per($num,$mult_pai)  // = false))
+    { // 获取???
+        if ($num < 0 || $num > $this->d3_total_Period + 1)
+            return 1;
+        
+        $r = $this->get_z_R_per();
+        
+        if (is_array($this->data_z_PI_per) && isset($this->data_z_PI_per[$num])) {
+            $this->data_z_PI_per[$num] = ( $r  + 1 ) * $mult_pai;
+        }
+        return $this->data_z_PI_per[$num];  // 不用return 数字的 ????
+    }
+    
+    
+    private function get_z_PI_oer($num)
+    { // 获取???
+        if ($num < 0 || $num > $this->d3_total_Period + 1)
+            return 1;
+        if (is_array($this->data_z_PI_per) && isset($this->data_z_PI_per[$num])) {
+            return $this->data_z_PI_per[$num];
+        }
+        return 1;
+    }
+    
+    
     
     
     
@@ -137,45 +164,43 @@ class TheRates
     private function fix_z_R( $x, $real_day_rate, $days, $len,$useSelfDay = false)
     {
         if ($useSelfDay) {
-            $this->set_z_R($x, $days * $real_day_rate); // real_rate / 360.0;
+            $this->set_z_R_day($x, $days * $real_day_rate); // real_rate / 360.0;
             return;
         }
         
         if ($x == 1 || $x == $this->d3_total_Period) {
-            $this->set_z_R($x, $days * $real_day_rate); // real_rate / 360.0;
+            $this->set_z_R_day($x, $days * $real_day_rate); // real_rate / 360.0;
             return;
         }
         
         if ($len > 0) {
-            $this->set_z_R($x, $len * $real_day_rate);
+            $this->set_z_R_day($x, $len * $real_day_rate);
         }
         if ($len == 0) {
-            $this->set_z_R($x, 15 * $real_day_rate);
+            $this->set_z_R_day($x, 15 * $real_day_rate);
         }
         if ($len < 0) {
-            $this->set_z_R($x, 30 * (- $len) * $real_day_rate);
+            $this->set_z_R_day($x, 30 * (- $len) * $real_day_rate);
         }        
     }
     
-    private function cal_PerRate($useSelfDay = false){ // 算月还,是按期,还是按天
+    private function cal_PerRate(){ // 算月还,是按期,还是按天
         
         $num = $this->getCount();
         
-        $this->sum_z_pai = 0; // 从 2 到 第 25 个 z_pai 求和
+        $this->sum_z_PI_day = 0; // 从 2 到 第 25 个 z_pai 求和
+        $this->sum_z_PI_per = 0;
         
         
-        if ( $useSelfDay ) {
+        
+        for ($x = $num; $x >= 1; $x --) {
+            $mult_pai = $this->get_z_PI_day($x + 1);
+            $this->set_z_PI_day($x, $mult_pai);
+            $this->sum_z_PI_day = $this->sum_z_PI_day + $mult_pai; // 从 2 到 第 25 个 z_pai 求和
+        }
+        
+        $this->first_z_PI_day = $this->get_z_PI_day(1); //  第1 个 z_pai
             
-            for ($x=$num; $x >= 1; $x--)
-            {
-                $mult_pai = $this->get_z_pai( $x+1 );
-                $this->set_z_pai( $x,$mult_pai,$useSelfDay  );
-                $this->sum_z_pai = $this->sum_z_pai + $mult_pai; // 从 2 到 第 25 个 z_pai 求和
-            }
-            
-            $this->first_z_pai = $this->get_z_pai(1); //  第1 个 z_pai
-            
-        } else {
             /*
             $mult_pai = 1;
             for ($x=$num; $x >= 1; $x--)
@@ -187,21 +212,24 @@ class TheRates
             $this->first_z_pai = $mult_pai; //  第1 个 z_pai
             */
 
-            for ($x=$num; $x >= 1; $x--)
-            {
-                $mult_pai = $this->get_z_pai( $x+1 );
-                $this->set_z_pai( $x,$mult_pai,$useSelfDay );
-                $this->sum_z_pai = $this->sum_z_pai + $mult_pai; // 从 2 到 第 25 个 z_pai 求和
-            }
-            
-            $this->first_z_pai = $this->get_z_pai(1); //  第1 个 z_pai
-            
+        for ($x = $num; $x >= 1; $x --) {
+            $mult_pai = $this->get_z_PI_per($x + 1);
+            $this->set_z_PI_per($x, $mult_pai);
+            $this->sum_z_PI_per = $this->sum_z_PI_per + $mult_pai; // 从 2 到 第 25 个 z_pai 求和
         }
+        
+        $this->first_z_PI_per = $this->get_z_PI_per(1); //  第1 个 z_pai
+            
+        
         
     }
     
-    public function cal_Period_Amount($total) {
-        $amt = $total * $this->first_z_pai / $this->sum_z_pai; // 求精确月供
+    public function cal_Period_Amount($total,$useSelfDay = false) {
+        if ( $useSelfDay ) {
+            $amt = $total * $this->first_z_PI_day / $this->sum_z_PI_day; // 求精确月供
+        } else {
+            $amt = $total * $this->first_z_PI_per / $this->sum_z_PI_per; // 求精确月供
+        }
         //$amt = round( $amt, 2, PHP_ROUND_HALF_UP ); // 求四舍五入到分月供
         // $this->d6_period_amount_round = round( ceil($this->d6_period_amount *100) / 100, 2, PHP_ROUND_HALF_UP ); // 求向上取整到分月供
         return $amt;
@@ -212,7 +240,7 @@ class TheRates
             return 0;
         
         if ( $useSelfDay ) { // 按天
-            $intD = $total_amt * $this->get_z_R($num);
+            $intD = $total_amt * $this->get_z_R_day($num);
         } else {
             $intD = $total_amt * $this->get_z_R_per(  );
         }
@@ -251,7 +279,7 @@ class TheRates
         }
         
         $this->set_z_R_per( $real_day_rate, $len );
-        $this->cal_PerRate($useDay);
+        $this->cal_PerRate();
         
         
         
@@ -277,8 +305,8 @@ class TheRates
                 
                 $echoStr = $echoStr."        <td>".$x."</td>\n";
                 //            $echoStr = $echoStr."        <td>".date_format($this->data_start_date,"Y-m-d")."</td>\n";
-                $echoStr = $echoStr."        <td>".$this->data_z_pai[$x]."</td>\n";
-                $echoStr = $echoStr."        <td>".$this->data_z_R[$x]."</td>\n";
+                $echoStr = $echoStr."        <td>".$this->data_z_PI_day[$x]."</td>\n";
+                $echoStr = $echoStr."        <td>".$this->data_z_R_day[$x]."</td>\n";
                
                 $echoStr = $echoStr."\n";
                 
@@ -286,7 +314,7 @@ class TheRates
                 $echoStr = $echoStr."    </tr>\n";
             }
             $echoStr = $echoStr."</table>\n";
-            $echoStr = $echoStr."_".$this->first_z_pai."_".$this->sum_z_pai."<br>\n";
+            $echoStr = $echoStr."_".$this->first_z_PI_day."_".$this->sum_z_PI_day."<br>\n";
             
             
             echo $echoStr;
